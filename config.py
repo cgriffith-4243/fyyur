5f233f91636dddbe4db086b7a1e85245849257dd
Cameron Griffith <cameron.c.griffith@gmail.com>
2020-09-26 01:14:08 -0700

    updated config / requirements

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,5 +8,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgres://{}@{}/{}'.format('cgriffith', 'localhost:5432', 'fyyur')
+SQLALCHEMY_DATABASE_URI = 'postgres://{}@{}/{}'.format(getpass.getuser(), '127.0.0.1:5432', 'fyyur')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
